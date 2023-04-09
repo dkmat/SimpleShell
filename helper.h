db9d1a99b3640b7068e81180027b583869c37544
Daksh Mathur <dmathur@ucdavis.edu>
2023-04-08 23:01:58 -0700

    new file:   helper.h 	modified:   sshell.c 	modified:   sshell.exe

---
+++ helper.h
@@ -0,0 +1,56 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CMDLINE_MAX 512
+
+/*
+This file contains all the functions used to organize 
+the different features of the shell program.
+*/
+
+int start();
+
+int start(){
+    char cmd[CMDLINE_MAX];
+
+        while (1) {
+                char *nl;
+                int retval;
+
+                /* Print prompt */
+                printf("sshell$ ");
+                fflush(stdout);
+
+                /* Get command line */
+                fgets(cmd, CMDLINE_MAX, stdin);
+
+                /* Print command line if stdin is not provided by terminal */
+                if (!isatty(STDIN_FILENO)) {
+                        printf("%s", cmd);
+                        fflush(stdout);
+                }
+
+                /* Remove trailing newline from command line */
+                nl = strchr(cmd, '\n');
+                if (nl)
+                        *nl = '\0';
+
+                /* Builtin command */
+                if (!strcmp(cmd, "exit")) {
+                        fprintf(stderr, "Bye...\n");
+                        break;
+                }
+
+                /* Regular command */
+                retval = system(cmd);
+                fprintf(stdout, "Return status value for '%s': %d\n",
+                        cmd, retval);
+        }
+
+        return EXIT_SUCCESS;
+}
+#endif
